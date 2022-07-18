585f24fa5011736ca16c183e9e6f253b917d9dc8
vishalsingh17 <vishal170997@gmail.com>
2022-07-18 15:44:09 +0530

    new file added

---
+++ js/index.js
@@ -0,0 +1,17 @@
+console.time('The code took');     //time starts
+console.log('Inside index.js');        //string
+console.log(17);                 //integer
+console.log(true); //boolean
+console.log([1,2,3,4, 'hi']);          //array
+console.log({f_name:'Vishal', l_name:'Singh'});       //object                
+console.table({column1: 'value1', column2: 'value2', column3: 'value3'}); //tabular object
+console.warn('This is a warning'); //warning in console
+//console.clear(); //clears console
+//console.timeEnd('The code took');       //time ends
+console.timeLog('The code took');    //logs the time taken
+console.assert(500<150, 'Age >150 is not possible'); //checks the age
+
+/* this
+ is 
+ a multiline
+ comment */
